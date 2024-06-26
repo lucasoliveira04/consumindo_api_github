abb072972ce77de159e3574495c6b7460e38e1bb
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-06-26 07:04:35 -0300

    finish

--- src/main/java/org/consumir_api/App.java
+++ src/main/java/org/consumir_api/App.java
@@ -1,13 +1,55 @@
 package org.consumir_api;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Username: ");
+        String username = scanner.nextLine();
+
+        String token = "";
+
+        connection(username, token);
+    }
+
+    private static void connection(String username, String token){
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.github.com/user/repos"))
+                .header("Authorization", "Bearer " + token)
+                .build();
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                JSONArray jsonArray = new JSONArray(response.body());
+                System.out.println("Repositorios do " + username + " : ");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject obj = jsonArray.getJSONObject(i);
+                    String name = obj.getString("name");
+                    System.out.println(name);
+                }
+            } else {
+                System.out.println(response.statusCode());
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
